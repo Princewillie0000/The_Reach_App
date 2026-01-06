eaa0ca60631f6651a6197ba5b962e24051022f4e
APPLE <apple@macbookpro.home>
2026-01-05 18:45:50 -0800

    Completed the onboarding for all screens

--- .next/dev/static/chunks/app_page_tsx_15edbb3b._.js
+++ .next/dev/static/chunks/app_page_tsx_15edbb3b._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_f4d154af._.js"
+  "static/chunks/_716e2b88._.js"
 ],
     source: "dynamic"
 });
--- .next/dev/static/development/_buildManifest.js
+++ .next/dev/static/development/_buildManifest.js
@@ -5,19 +5,7 @@ self.__BUILD_MANIFEST = {
     "fallback": []
   },
   "sortedPages": [
-    "/Landing",
-    "/NotificationPage",
-    "/PropertyDetail",
-    "/WalletPage",
     "/_app",
-    "/_error",
-    "/auth/BuyerRegistration",
-    "/auth/CreatorRegistration",
-    "/auth/DeveloperRegistration",
-    "/auth/RoleSelection",
-    "/auth/VerificationFlow",
-    "/dashboards/BuyerDashboard",
-    "/dashboards/CreatorDashboard",
-    "/dashboards/DeveloperDashboard"
+    "/_error"
   ]
 };self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
